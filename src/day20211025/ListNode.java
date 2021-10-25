79b60c90a7d27728edacd9195301b9f4b0124d79
Jiangzuizui <576163075@qq.com>
2021-10-25 23:49:58 +0800

    20211025 LinkedList

---
+++ src/day20211025/ListNode.java
@@ -0,0 +1,18 @@
+package day20211025;
+
+import java.util.List;
+
+public class ListNode {
+    private boolean val;
+
+    public ListNode removeElements(ListNode head, int val){
+        if(head==null){
+            return head;
+        }
+        ListNode cur=head;
+        ListNode prev=null;
+        while(cur!=null){
+
+
+    }
+}
