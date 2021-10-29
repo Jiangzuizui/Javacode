c6f06260fed1b51ae8417ffdfc62cef1a20002cf
Jiangzuizui <576163075@qq.com>
2021-10-29 22:14:06 +0800

    20211029 LinkedList

--- src/day20211027/LinkedList.java
+++ src/day20211027/LinkedList.java
@@ -64,7 +64,48 @@ public class LinkedList<E> {
 
     //任意位置插入,第一个数据结点为0号下标
     public boolean addIndex(int index,E e){
+        //0.参数进行检测
+        if(!(index>=0 && index<=size)){
+            throw new IndexOutOfBoundsException("addIndex:下标越界");
+        }
+        if(index==size){
+            addLast(e);
+        }else{
+            //1.找到index对应的结点
+            //方法一 O(N)
+            /*ListNode<E> cur=first;
+            for(int i=0;i<index;i++){
+                cur=cur.next;
+            }*/
 
+            //方法二 O(N/2)
+            ListNode<E> cur=null;
+            if(index<((size)>>1)){
+                cur=first;
+                for(int i=0;i<index;i++){
+                    cur=cur.next;
+                }
+            }else{
+                cur=last;
+                for(int i=size-1;i>index;i--){
+                    cur=cur.prev;
+                }
+            }
+
+            if(cur==first){
+                addFirst(e);
+            }else{
+                ListNode<E> newNode=new ListNode<>(e);
+
+                //2.插入新结点
+                newNode.prev=cur.prev;
+                newNode.next=cur;
+                newNode.prev.next=newNode;
+                cur.prev=newNode;
+
+                size++;
+            }
+        }
         return true;
     }
 
@@ -115,8 +156,64 @@ public class LinkedList<E> {
         return first==null;
     }
 
-    public E remove(E e){
-        return null;
+    //头删
+    public void removeFirst(){
+        if(first==null){
+            return;
+        }else if(first==last){
+            //链表中只有一个结点
+            first=null;
+            last=null;
+            //size--;
+        }else{
+            //至少有两个结点
+            first=first.next;
+            first.prev=null;
+            //size--;
+        }
+        size--;
+    }
+
+    //尾删
+    public void removeLast(){
+        if(first==null){
+            return;
+        }else if(first==last){
+            first=null;
+            last=null;
+            //size--;
+        }else{
+            last=last.prev;
+            last.next=null;
+            //size--;
+        }
+        size--;
+    }
+
+    public boolean remove(E e){
+        //找到e对应的结点
+        ListNode<E> cur=first;
+        while(cur!=null){
+            if(e.equals(cur.elem)){
+                break;
+            }
+            cur=cur.next;
+        }
+        if(cur==null){
+            return false;
+        }
+        //删除该节点
+        if(cur==first){
+            //头删
+            removeFirst();
+        }else if(cur==last){
+            removeLast();
+        }else{
+            cur.prev.next=cur.next;
+            cur.next.prev=cur.prev;
+            size--;
+        }
+        return true;
     }
 
     @Override
@@ -144,17 +241,31 @@ public class LinkedList<E> {
         System.out.println(list);
 
         //尾插
-        /*list.addLast(1);
+        list.addLast(1);
         list.addLast(2);
         list.addLast(3);
-        list.addLast(4);*/
+        list.addLast(4);
+        list.addLast(5);
 
         //头插
+        /*list.addFirst(5);
         list.addFirst(4);
         list.addFirst(3);
         list.addFirst(2);
-        list.addFirst(1);
+        list.addFirst(1);*/
 
         System.out.println(list);
+        list.addIndex(5,6);//123456
+        System.out.println(list);
+
+        list.addIndex(0,0);//0123456
+        list.addIndex(2,7);//01723456
+        System.out.println(list);
+
+        list.remove(0);
+        list.remove(6);
+        list.remove(7);
+        System.out.println(list);
+
     }
 }
