e926505c6aab097448d725b47cc444cdb0115e45
Jiangzuizui <576163075@qq.com>
2021-10-30 23:06:34 +0800

    20211030 Stack

---
+++ src/day20211030/Solution2.java
@@ -0,0 +1,96 @@
+package day20211030;
+
+import java.util.Stack;
+
+public class Solution2 {
+    public boolean isValid(String s){
+        Stack<Character> st=new Stack<>();
+        for(int i=0;i<s.length();i++){
+            //检测第i个括号是左还是右
+            char c=s.charAt(i);
+            if(c=='('||c=='['||c=='{'){
+                //左括号,入栈
+                st.push(c);
+            }else{
+                //右括号,将其与栈顶元素相比
+                if(st.empty()){
+                    return false;
+                }
+                //获取栈顶的左括号
+                char top=st.peek();//peek只会将栈顶的元素获取到,并不会删除
+                if((top=='('&&c==')') ||
+                    (top=='['&&c==']') ||
+                    (top=='{'&&c=='}')){
+                        st.pop();
+                        continue;
+                }else{
+                    return false;
+                }
+            }
+        }
+        //如果栈中不为空,说明左括号多了
+        if(!st.empty()){
+            return false;
+        }
+        return true;
+    }
+
+    public int evalRPN(String[] tokens){
+        Stack<Integer> s=new Stack<>();
+        for(String e:tokens){
+            //char ch=e.charAt(0);
+            //if(!((ch=='+')||(ch=='-')
+              //      ||(ch=='*')||(ch=='/'))){
+            //这种方式测不到负数的情况,会把负号当做减号
+            if(!((e.equals("+"))||(e.equals("-"))||
+                    (e.equals("*"))||(e.equals("/")))){
+            //e是数字字符串--先将数字字符串转为数字,然后入栈
+                s.push(Integer.parseInt(e));
+            }else{
+                //e是运算符
+                //从栈顶取两个操作数进行运算
+                int right=s.pop();
+                int left=s.pop();
+                switch(e.charAt(0)){
+                    case '+':
+                        s.push(left+right);
+                        break;
+                    case '-':
+                        s.push(left-right);
+                        break;
+                    case '*':
+                        s.push(left*right);
+                        break;
+                    case '/':
+                        s.push(left/right);
+                        break;
+                }
+            }
+        }
+        return s.peek();
+    }
+
+    public boolean IsPopOrder(int[] pushA,int[] popA){
+        Stack<Integer> s=new Stack<>();
+        int pushIdx=0,outIdx=0;
+
+        while(outIdx<popA.length) {
+            while (s.empty() || s.peek() != popA[outIdx]) {
+                //有元素时,继续入栈
+                if (pushIdx < pushA.length) {
+                    s.push(pushA[pushIdx]);
+                    pushIdx++;
+                } else {
+                    return false;
+                }
+            }
+            s.pop();
+            outIdx++;
+        }
+        return true;
+    }
+
+    public static void main(String[] args) {
+
+    }
+}
---
+++ src/day20211030/TestQueue.java
@@ -0,0 +1,28 @@
+package day20211030;
+
+import java.util.LinkedList;
+import java.util.Queue;
+
+public class TestQueue {
+
+    public static void main(String[] args) {
+        Queue<Integer> q=new LinkedList<>();
+        q.offer(1);
+        q.offer(2);
+        q.offer(3);
+        q.offer(4);
+        q.offer(5);
+        q.offer(6);
+
+        System.out.println(q.size());
+        System.out.println(q.peek());//获取队头元素
+
+        q.poll();
+        q.poll();
+
+        System.out.println(q.size());
+        System.out.println(q.peek());
+
+
+    }
+}
