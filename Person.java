76c4d03cafc9405fabdea33c9bc3240898831647
Jiangzuizui <83112910+Jiangzuizui@users.noreply.github.com>
2021-12-03 23:09:48 +0800

    Add files via upload

--- Person.java
+++ Person.java
@@ -1,6 +1,20 @@
 package day20211121;
 
-public class Person {
+public class Person implements Comparable<Person> {
+    public  Person(){
+
+    }
+
+    public Person(int age){
+        this.age=age;
+    }
+
+    @Override
+    public int compareTo(Person o) {
+        //return age-o.age;//升序
+        return o.age-age;//降序
+    }
+
     String name;
     String gender;
     int age;
--- Sort.java
+++ Sort.java
@@ -1,9 +1,12 @@
 package day20211121;
 
+import java.util.Comparator;
+
 public class Sort {
-    //泛型冒泡
+    //泛型冒泡排序
     public static <E extends Comparable<E>> void bubbleSort(E[] array){
         int size=array.length;
+
         for (int i = 0; i < size-1; i++) {
             boolean isChange=false;
             for (int j = 1; j < size-1; j++) {
@@ -18,5 +21,51 @@ public class Sort {
                 return;
             }
         }
+    }
+
+    public static <E> void bubbleSort(E[] array, Comparator<E> com){
+        int size=array.length;
+
+        for (int i = 0; i < size-1; i++) {
+            boolean isChange=false;
+            for (int j = 1; j < size-1; j++) {
+                if(com.compare(array[j-1],array[j])>0){
+                    E tmp=array[j-1];
+                    array[j-1]=array[j];
+                    array[j]=tmp;
+                    isChange=true;
+                }
+            }
+            if(!isChange){
+                return;
+            }
+        }
+    }
+
+
+    public static void main(String[] args) {
+        Integer arr[]={3,2,7,6,1,0,5};
+        Sort.bubbleSort(arr);
+
+        String arr1[]={"222","111","333"};
+        Sort.bubbleSort(arr1);
+
+        Person[] p={new Person(10),new Person(17),new Person(12)};
+        //Sort.bubbleSort(p);
+
+        Sort.bubbleSort(p, new Comparator<Person>() {
+            @Override
+            public int compare(Person o1, Person o2) {
+                return o1.age-o2.age;//升序
+            }
+        });
+        System.out.println();
+
+        Sort.bubbleSort(p, new Comparator<Person>() {
+            @Override
+            public int compare(Person o1, Person o2) {
+                return o2.age-o1.age;
+            }
+        });
     }
 }
